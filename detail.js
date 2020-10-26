db03e9cb32f7d31bc4af097ced0a5fe9b5028a1d
Mathilde Åkerlind <mathilde.akerlind@gmail.com>
2020-10-26 16:42:06 +0100

    nästan klar detaljsida

--- detail.js
+++ detail.js
@@ -1,17 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    let button2 = document.getElementById("button2");
+    let guestBookBtn = document.getElementById("guestBookBtn");
     let textInput = document.getElementById("textInput");
-    let divId = document.getElementById("divId");
-    let newRow = document.createElement("tr");
-    button2.addEventListener("click", function(e) {
 
-        console.log("HELLO");
+    let guestBookText = document.getElementById("guestBookText");
+    guestBookBtn.addEventListener("click", function(e) {
+
+
         nyText = textInput.value;
-        let newRow = document.createElement("tr");
+        let newRow = document.createElement("p");
         console.log(nyText);
 
         newRow.append(nyText);
-        GBtext.append(newRow);
+        guestBookText.append(newRow);
+        textInput.value = "";
 
     })
 
