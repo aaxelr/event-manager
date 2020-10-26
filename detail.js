3aaff947ad06b0dba2b8e6be84765e885604363a
Mathilde Åkerlind <mathilde.akerlind@gmail.com>
2020-10-26 14:53:47 +0100

    detaljsidan börjar bli klar, ska kollas av resten i team bara

--- detail.js
+++ detail.js
@@ -2,13 +2,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
     let button2 = document.getElementById("button2");
     let textInput = document.getElementById("textInput");
     let divId = document.getElementById("divId");
-
+    let newRow = document.createElement("tr");
     button2.addEventListener("click", function(e) {
 
         console.log("HELLO");
         nyText = textInput.value;
+        let newRow = document.createElement("tr");
         console.log(nyText);
-        GBtext.append(nyText);
+
+        newRow.append(nyText);
+        GBtext.append(newRow);
+
     })
 
 })
