6c308fda86391abb9f424dc2164f437fe1512057
Georgios <test@example.com>
2020-10-26 23:28:43 +0100

    option and date menu choice in html file and som styling on that

--- sort.js
+++ sort.js
@@ -1,13 +1,13 @@
 var mydate = [
-    "2016-10-01",
+    "2019-10-01",
     "2016-09-13",
-    "2016-09-05",
+    "2014-09-05",
     "2016-09-09",
     "2016-10-02"
-  ];
-  mydate.sort(function(a, b) {
+];
+mydate.sort(function(a, b) {
     var a1 = a.split('-'),
-      b1 = b.split('-');
+        b1 = b.split('-');
     return new Date(a1[0], a1[1], a1[2]) - new Date(b1[0], b1[1], b1[2]);
-  });
-  console.log(mydate);
+});
+console.log(mydate);
