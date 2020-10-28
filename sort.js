68b8804b629a391dfb955741dafc2d46eb3976f9
Georgios <test@example.com>
2020-10-28 10:46:52 +0100

    lagt till idé för event

--- sort.js
+++ sort.js
@@ -17,25 +17,45 @@ document.addEventListener('DOMContentLoaded', function(e) {
     let eventArray = []
     let eventBtn = document.getElementById("eventBtn");
 
-    eventBtn.addEventListener("click", function (e) {
-        
-    
-    for (let i = 1; i <= localStorage.length; i++){
-        let event = JSON.parse(window.localStorage.getItem('event' + [i]));;
-        eventArray.push(event);
-    }  
-    for (let i = 0; i <= localStorage.length; i++) {
-        console.log(eventArray[i].date);
-    } 
-    createEvents();
-})
+    eventBtn.addEventListener("click", function(e) {
 
-function createEvents () {
-    for (let i = 1; i <= localStorage.length; i++) {
-        const eventDiv = document.createElement("div");
-        eventDiv.setAttribute("class", eventArray[i].date);
-    }
-}
+        let myJSON = '{ "category":"Bröllop", "date":"2020-10-21", "name":"bröllop", "text":"Nu var det dax igen" }';
+        let myObj = JSON.parse(myJSON);
+        document.getElementById("event-category").innerHTML = myObj.category;
+        document.getElementById("event-date").innerHTML = myObj.date;
+        document.getElementById("event-name").innerHTML = myObj.name;
+        document.getElementById("event-text").innerHTML = myObj.text;
+
+
+        function myFunction() {
+            var para = document.createElement("P");
+            para.innerHTML = "This is a paragraph.";
+            document.getElementById("myDIV").appendChild(para);
+        }
+
+        myFunction();
+
+
+        // // for (let i = 1; i <= localStorage.length; i++) {
+        // //     let event = JSON.parse(window.localStorage.getItem('event' + [i]));;
+        // //     eventArray.push(event);
+        // // }
+        // // for (let i = 0; i <= localStorage.length; i++) {
+        // //     console.log(eventArray[i].date);
+        // // }
+        // // createEvents();
+        // document.getElementById("result").innerHTML = localStorage.getItem("event2");
+
+
+    })
+
+    // // function createEvents() {
+    // //     for (let i = 1; i <= localStorage.length; i++) {
+    // //         const eventDiv = document.createElement("div");
+    // //         eventDiv.setAttribute("class", info);
+
+    // //     }
+    // // }
 
     /*let titleArray = [];
     let dateArray = [];
@@ -56,6 +76,4 @@ function createEvents () {
 
     })*/
 
-
-
-})
+});
