f3257a1c25dea08edf11db953c5ac8d6af3e6de4
Sebastian Carlson <sebcar98@gmail.com>
2020-10-28 12:17:24 +0100

    försöker fixa en bug med forlopen i sort.js

--- sort.js
+++ sort.js
@@ -1,65 +1,60 @@
-/*var mydate = [
-    "2019-10-01",
-    "2016-09-13",
-    "2014-09-05",
-    "2016-09-09",
-    "2016-10-02"
-];
-mydate.sort(function(a, b) {
-    var a1 = a.split('-'),
-        b1 = b.split('-');
-    return new Date(a1[0], a1[1], a1[2]) - new Date(b1[0], b1[1], b1[2]);
-});
-console.log(mydate);*/
-
 document.addEventListener('DOMContentLoaded', function (e) {
 
-    let eventArray = []
+    let eventArray = [];
     let eventCount = 1;
     let eventBtn = document.getElementById("eventBtn");
 
     eventBtn.addEventListener("click", function (e) {
-        createEventDiv(eventCount);
-        eventCount++;
-        
-        for (let i = 1; i <= localStorage.length; i++) {
+        for (let i = eventCount; i <= localStorage.length; i++) {
             let event = JSON.parse(window.localStorage.getItem('event' + [i]));;
             eventArray.push(event);
         }
-        for (let i = 0; i <= localStorage.length; i++) {
-            console.log(eventArray[i].date);
+        createEventDiv(eventCount);
+
+        let h2 = document.getElementById("title" + [eventCount]);
+        let p1 = document.getElementById("p-date" + [eventCount]);
+        let p2 = document.getElementById("p-category" + [eventCount]);
+
+        for (let i = 0; i < localStorage.length; i++) {
+            h2.innerHTML = eventArray[i].name;
+            p1.innerHTML = eventArray[i].date;
+            p2.innerHTML = eventArray[i].category;
         }
+
+        console.log(eventCount);
+        eventCount++;
+        
+        //console.log(eventArray[0].date);
+
+
     })
     function createEventDiv(eventCount) {
-        
         let createEventDiv = document.getElementById("create-event-bars");
 
-        let divTag = document.createElement("div");
-        divTag.setAttribute("id", "demoClass" + [eventCount]);
-        createEventDiv.appendChild(divTag);
+        let createDivTag = document.createElement("div");
+        createDivTag.setAttribute("id", "demoClass" + [eventCount]);
+        createEventDiv.appendChild(createDivTag);
 
-        let headerTag = document.createElement("h2");
-        headerTag.setAttribute("id", "header" + [eventCount]);
-        let pTag = document.createElement("p");
-        pTag.setAttribute("id", "paragraf" + [eventCount]);
-        let pTag2 = document.createElement("p");
-        pTag2.setAttribute("id", "paragraf" + [eventCount]);
+        let createHeaderTag = document.createElement("h2");
+        createHeaderTag.setAttribute("id", "title" + [eventCount]);
+        let CreatePTag = document.createElement("p");
+        CreatePTag.setAttribute("id", "p-date" + [eventCount]);
+        let createPTag2 = document.createElement("p");
+        createPTag2.setAttribute("id", "p-category" + [eventCount]);
 
-        divTag.appendChild(headerTag);
-        divTag.appendChild(pTag);
-        divTag.appendChild(pTag2);
+        createDivTag.appendChild(createHeaderTag);
+        createDivTag.appendChild(CreatePTag);
+        createDivTag.appendChild(createPTag2);
 
         console.log("check if it works");
     }
 
-    function fillEventDiv() {
-
-    }
-
-    
 
 });
- // let myJSON = '{ "category":"Bröllop", "date":"2020-10-21", "name":"bröllop", "text":"Nu var det dax igen" }';
+
+
+
+// let myJSON = '{ "category":"Bröllop", "date":"2020-10-21", "name":"bröllop", "text":"Nu var det dax igen" }';
         // let myObj = JSON.parse(myJSON);
         // document.getElementById("event-category").innerHTML = myObj.category;
         // document.getElementById("event-date").innerHTML = myObj.date;
@@ -74,21 +69,35 @@ document.addEventListener('DOMContentLoaded', function (e) {
     //     document.getElementById("myDIV").appendChild(para);
     // }
 
-    /*let titleArray = [];
-    let dateArray = [];
-    let categoryArray = [];
+/*let titleArray = [];
+let dateArray = [];
+let categoryArray = [];
 
-    eventBtn.addEventListener("click", function(e) {
-        for (let i = 1; i < 3; i++) {
-            titleArray.push(document.getElementById("event-title" + [i]).innerHTML);
-            dateArray.push(document.getElementById("event-date" + [i]).innerHTML);
-            categoryArray.push(document.getElementById("event-date" + [i]).innerHTML);
-        }
-        dateArray.sort(function(a, b) {
-            var a1 = a.split('-'),
-                b1 = b.split('-');
-            return new Date(a1[0], a1[1], a1[2]) - new Date(b1[0], b1[1], b1[2]);
-        });
-        console.log(dateArray);
+eventBtn.addEventListener("click", function(e) {
+    for (let i = 1; i < 3; i++) {
+        titleArray.push(document.getElementById("event-title" + [i]).innerHTML);
+        dateArray.push(document.getElementById("event-date" + [i]).innerHTML);
+        categoryArray.push(document.getElementById("event-date" + [i]).innerHTML);
+    }
+    dateArray.sort(function(a, b) {
+        var a1 = a.split('-'),
+            b1 = b.split('-');
+        return new Date(a1[0], a1[1], a1[2]) - new Date(b1[0], b1[1], b1[2]);
+    });
+    console.log(dateArray);
 
-    })*/
+})*/
+
+/*var mydate = [
+    "2019-10-01",
+    "2016-09-13",
+    "2014-09-05",
+    "2016-09-09",
+    "2016-10-02"
+];
+mydate.sort(function(a, b) {
+    var a1 = a.split('-'),
+        b1 = b.split('-');
+    return new Date(a1[0], a1[1], a1[2]) - new Date(b1[0], b1[1], b1[2]);
+});
+console.log(mydate);*/
